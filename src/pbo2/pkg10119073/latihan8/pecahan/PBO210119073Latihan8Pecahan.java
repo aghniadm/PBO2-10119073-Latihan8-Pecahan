0aa778feee360c691f1b878de79e9d175d47ed98
aghniadm <aghniadewii@gmail.com>
2020-10-18 18:46:08 +0700

    PBO2-10119073-Latihan8-Pecahan

---
+++ src/pbo2/pkg10119073/latihan8/pecahan/PBO210119073Latihan8Pecahan.java
@@ -0,0 +1,24 @@
+package pbo2.pkg10119073.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * pecahan
+ *
+ */
+public class PBO210119073Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
